331dc3af5ead9e0116f0d0dd362f0ecea0aec385
BenitezAxel <benitezaxel03@gmail.com>
2020-11-21 19:46:01 -0300

    cambios

--- barrio31/src/App.js
+++ barrio31/src/App.js
@@ -2,15 +2,7 @@ import React from 'react';
 import Mapa from  './Components/Mapa.js';
 
 
-export default function App() {
-    const [viewport, setVieport] = useState({
-        lalitude: 52.6376,
-        longitude: -1.135171,
-        width:"100vw",
-        height: "100vh",
-        zoom: 12
-    })
-
+function App() {
   return (
     <div>
       <Mapa/>
