3ab3a7d7d1148a59076cd1a6b0f600375e49c058
BenitezAxel <benitezaxel03@gmail.com>
2020-11-21 19:01:18 -0300

    cambios

--- barrio31/src/App.js
+++ barrio31/src/App.js
@@ -1,27 +1,15 @@
-import React,{ useState } from 'react';
-import ReactMapGL from "react-map-gl";
+import React from 'react';
+import Mapa from  './Components/Mapa.js';
+import Mapa2 from './Components/Mapa2.js';
 
 
-
-export default function App(){
-  const [viewport, setViewport] = useState({
-    latitude: -34.58224781899445,
-    longitude: -58.3790560432247,
-    width: '100vw',
-    height: '100vh',
-    zoom: 15
-  });
-  
+function App() {
   return (
-  <div>
-    <ReactMapGL {...viewport}
-    mapboxApiAccessToken="pk.eyJ1IjoiYXhlbDAzIiwiYSI6ImNrZTR1emdybzB0d2QyeXBiYWg0cDJ3ODcifQ.bEVJT1JeOq7rh1bmhXRnjA"
-    mapStyle="mapbox://styles/axel03/cke4v99cb1jpk19mpma6mkowo"
-    onViewportChange={viewport =>{
-      setViewport(viewport);
-    }}>
-     
-    </ReactMapGL>
-  </div>
-  );
+    <div>
+      <Mapa/>
+      <Mapa2/>
+    </div>
+  )
 }
+
+export default App
---
+++ barrio31/src/Components/Mapa.js
@@ -0,0 +1,27 @@
+import React,{ useState } from 'react';
+import ReactMapGL from "react-map-gl";
+
+
+
+export default function Mapa(){
+  const [viewport, setViewport] = useState({
+    latitude: -34.58224781899445,
+    longitude: -58.3790560432247,
+    width: '80vw',
+    height: '80vh',
+    zoom: 15
+  });
+  
+  return (
+  <div>
+    <ReactMapGL {...viewport}
+    mapboxApiAccessToken="pk.eyJ1IjoiYXhlbDAzIiwiYSI6ImNrZTR1emdybzB0d2QyeXBiYWg0cDJ3ODcifQ.bEVJT1JeOq7rh1bmhXRnjA"
+    mapStyle="mapbox://styles/axel03/cke4v99cb1jpk19mpma6mkowo"
+    onViewportChange={viewport =>{
+      setViewport(viewport);
+    }}>
+     
+    </ReactMapGL>
+  </div>
+  );
+}
