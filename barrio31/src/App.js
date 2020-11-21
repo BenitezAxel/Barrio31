cd27864aaa2afe4f08a577f84f95a7e9e053fc63
b nnn <castilloo.mariel@gmail.com>
2020-11-21 19:04:43 -0300

    prueba del mapa

--- barrio31/src/App.js
+++ barrio31/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+export default function App() {
+    const [viewport, setVieport] = useState({
+        lalitude: 52.6376,
+        longitude: -1.135171,
+        width:"100vw",
+        height: "100vh",
+        zoom: 12
+    })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
-
-export default App;
