753d8ca0ea6e1fd375a877605aa84dfef899ba70
BenitezAxel <benitezaxel03@gmail.com>
2020-11-21 19:42:57 -0300

    cambios

--- barrio31/src/App.js
+++ barrio31/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Mapa from  './Components/Mapa.js';
-import Mapa2 from './Components/Mapa2.js';
 
 
 function App() {
   return (
     <div>
       <Mapa/>
-      <Mapa2/>
     </div>
   )
 }
